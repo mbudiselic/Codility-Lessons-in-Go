f329f53584035c3532a42f9f7d6f683859230848
Marko Budiselić <markobudiselic@gmail.com>
2015-08-07 23:07:54 +0200

    Passing but may need improvements...

---
+++ Lesson 02/PermCheck.go
@@ -0,0 +1,64 @@
+/*
+A non-empty zero-indexed array A consisting of N integers is given.
+A permutation is a sequence containing each element from 1 to N once, and only once.
+For example, array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+is a permutation, but array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+is not a permutation, because value 2 is missing.
+The goal is to check whether array A is a permutation.
+Write a function:
+func Solution(A []int) int
+that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+For example, given array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+the function should return 1.
+Given array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+the function should return 0.
+Assume that:
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [1..1,000,000,000].
+Complexity:
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+Elements of input arrays can be modified.
+*/
+
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+
+// you can use fmt.Println for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A []int) int {
+	// write your code in Go 1.4
+	N := len(A)
+	positions := make([]int, N)
+	var sum int
+	for _, v := range A {
+		if v > N {
+			return 0
+		}
+		if positions[v-1] != 1 {
+			positions[v-1] = 1
+			sum += v
+		}
+	}
+	if sum == (N*(N+1))/2 {
+		return 1
+	}
+	return 0
+}
