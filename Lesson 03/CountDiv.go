c8ca061dc7f11691287f60bd5e03f5802caa969a
mbudiselic <mbudiselic@users.noreply.github.com>
2015-08-15 22:55:37 +0200

    CountDiv solution

---
+++ Lesson 03/CountDiv.go
@@ -0,0 +1,39 @@
+/*
+Write a function:
+
+    func Solution(A int, B int, K int) int
+
+that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+
+    { i : A ≤ i ≤ B, i mod K = 0 }
+
+For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+
+Assume that:
+
+        A and B are integers within the range [0..2,000,000,000];
+        K is an integer within the range [1..2,000,000,000];
+        A ≤ B.
+
+Complexity:
+
+        expected worst-case time complexity is O(1);
+        expected worst-case space complexity is O(1).
+*/
+
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+
+// you can use fmt.Println for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A int, B int, K int) int {
+	// write your code in Go 1.4
+	var offset int
+	if A%K == 0 {
+		offset++
+	}
+	return B/K - A/K + offset
+}
