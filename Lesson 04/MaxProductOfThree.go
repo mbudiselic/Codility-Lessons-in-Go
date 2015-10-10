658fb5c3ada25ddca7e0e806b3ca0dc207296c81
mbudiselic <mbudiselic@users.noreply.github.com>
2015-10-10 21:40:50 +0200

    Correct 100% solution

--- Lesson 04/MaxProductOfThree.go
+++ Lesson 04/MaxProductOfThree.go
@@ -1,18 +1,29 @@
 package solution
 
-import "math"
+import "sort"
 
 func Solution(A []int) int {
 	N := len(A)
-	max := math.MinInt32
-	for P := 0; P < N-2; P++ {
-		for Q := P + 1; Q < N-1; Q++ {
-			for R := Q + 1; R < N; R++ {
-				if A[P]*A[Q]*A[R] > max {
-					max = A[P] * A[Q] * A[R]
-				}
-			}
+
+	if N == 3 {
+		return A[0] * A[1] * A[2]
+	}
+	sort.Ints(A)
+	//largest number is 0
+	if A[N-1] == 0 {
+		return 0
+	} else
+	//all negative
+	if A[N-1] < 0 {
+		return A[N-1] * A[N-2] * A[N-3]
+	} else {
+		//max is either product of 2 biggest negatives and largest positive
+		max := A[0] * A[1] * A[N-1]
+		//or product of 3 largest numbers
+		if A[N-1]*A[N-2]*A[N-3] > max {
+			return A[N-1] * A[N-2] * A[N-3]
+		} else {
+			return max
 		}
 	}
-	return max
 }
